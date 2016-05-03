cf4094f7524fb8304ae1f2987d0ee270e12ba351
Guillaume <guillaume.nougier@ynov.com>
2016-05-03 12:19:56 +0200

    simulation graphique

--- IoTNode/public/javascripts/GraphController.js
+++ IoTNode/public/javascripts/GraphController.js
@@ -2,23 +2,44 @@
  * Created by Guillaume on 02/05/2016.
  */
 
-var cpuDatas = [0,4,0,0,3,10,45,20,3,2,2,1,4,6,2];
-var ramDatas = [48,48,48,48,48,48,48,48,48,48,48,48,48,48,60];
-var diskDatas = [0,0,0,0,0,0,4,50,20,10,3, 0,0,0,0];
-var linkDatas = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var cpuDatas = [0, 4, 0, 0, 3, 10, 45, 20, 3, 2, 2, 1, 4, 6, 2];
+var ramDatas = [48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 48, 60];
+var diskDatas = [0, 0, 0, 0, 0, 0, 4, 50, 20, 10, 3, 0, 0, 0, 0];
+var linkDatas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var chart;
+var options = {
+    title: 'Company Performance',
+    curveType: 'function',
+    legend: { position: 'bottom' }
+};
+var baseArray = [];
+var cpt = 0;
 
-function updateDatasGraph(newCpuValue, newRamValue, newDiskValue, newLinkValue) {
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+function drawChart() {
+    updateDataGraph(50, 48, 30, 0);
+    var array = graphTab();
+    var data = google.visualization.arrayToDataTable(array);
+
+    chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+    chart.draw(data, options);
+}
+
+
+function updateDataGraph(newCpuValue, newRamValue, newDiskValue, newLinkValue) {
     if (cpuDatas.length >= 15) {
-        delete cpuDatas[0];
+        cpuDatas.splice(0, 1);
         cpuDatas.push(newCpuValue);
 
-        delete ramDatas[0];
+        ramDatas.splice(0, 1);
         ramDatas.push(newRamValue);
 
-        delete diskDatas[0];
+        diskDatas.splice(0, 1);
         diskDatas.push(newDiskValue);
 
-        delete linkDatas[0];
+        linkDatas.splice(0, 1);
         linkDatas.push(newLinkValue);
     }
     else {
@@ -29,10 +50,36 @@ function updateDatasGraph(newCpuValue, newRamValue, newDiskValue, newLinkValue) 
     }
 }
 
-function graphTab(){
+function graphTab() {
     var graphTab = [];
-    for(var i = 0; i < cpuDatas.length; i ++){
-        graphTab.push([i , cpuDatas[i], ramDatas[i], diskDatas[i], linkDatas[i]])
+    graphTab.push(['i', 'CPU', 'RAM', 'Disk', 'Link']);
+    for (var i = 0; i < cpuDatas.length; i++) {
+        graphTab.push([cpt, cpuDatas[i], ramDatas[i], diskDatas[i], linkDatas[i]])
+        cpt++;
     }
     return graphTab;
 }
+
+
+function reload(){
+
+    updateDataGraph(50, 48, 30, 0);
+    var array = graphTab();
+
+    console.log(array);
+    var data = google.visualization.arrayToDataTable(array);
+
+    chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+
+    chart.draw(data, options);
+}
+
+var i = 0;
+var interval = setInterval(function(){
+    reload();
+    i++;
+    if (i == 15){
+        clearInterval(interval)
+    }
+
+},1000);
